629b75406eb8687126b93d94b6e7c3b2d5fc7ec1
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-09-03 01:07:23 +0900

    no message

---
+++ src/main/java/com/lottomatic/springbootweb/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.lottomatic.springbootweb.domain;
+
+import lombok.Getter;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
