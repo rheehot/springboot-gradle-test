097f28e130793153da379a576f912635e0e6bfd1
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-09-03 01:07:04 +0900

    no message

--- src/main/java/com/lottomatic/springbootweb/SpringbootWebApplication.java
+++ src/main/java/com/lottomatic/springbootweb/SpringbootWebApplication.java
@@ -2,7 +2,9 @@ package com.lottomatic.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class SpringbootWebApplication {
 
